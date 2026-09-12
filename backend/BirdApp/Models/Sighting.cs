81198c6961fa8eccc272d3fe38a5d5d039ab0fe0
Maciej Hawrot <hawrotmaciej@gmail.com>
2026-09-12 21:04:00 +0200

    Add Sighting model to represent bird sighting data

---
+++ backend/BirdApp/Models/Sighting.cs
@@ -0,0 +1,13 @@
+namespace BirdApp.Models;
+
+public class Sighting
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? Notes { get; set; }
+    public int SpeciesId { get; set; }
+    public Species Species { get; set; } = null!;
+    public List<Photo> Photos { get; set; } = new();
+}
