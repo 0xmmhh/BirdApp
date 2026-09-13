fa3a6b187eda3c601ebb1357715457f5313947c3
Maciej Hawrot <hawrotmaciej@gmail.com>
2026-09-13 21:49:07 +0200

    Make `SpeciesId` nullable in `Sightings` table

---
+++ backend/BirdApp/Migrations/20260913150605_MakeSpeciesIdNullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BirdApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeSpeciesIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sightings_Species_SpeciesId",
+                table: "Sightings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpeciesId",
+                table: "Sightings",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sightings_Species_SpeciesId",
+                table: "Sightings",
+                column: "SpeciesId",
+                principalTable: "Species",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sightings_Species_SpeciesId",
+                table: "Sightings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpeciesId",
+                table: "Sightings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sightings_Species_SpeciesId",
+                table: "Sightings",
+                column: "SpeciesId",
+                principalTable: "Species",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
