a40df6b1299e0a7fa9f5fe9c58a4b3794861ab84
Maciej Hawrot <hawrotmaciej@gmail.com>
2026-09-13 22:06:19 +0200

    Add CORS policy to allow frontend access

--- backend/BirdApp/Program.cs
+++ backend/BirdApp/Program.cs
@@ -2,11 +2,21 @@ using BirdApp.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+const string corsPolicyName = "AllowFrontend";
 
 builder.Services.AddDbContext<BirdAppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
                       ?? "Data Source=Data/birdapp.db"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicyName, policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(corsPolicyName);
 app.UseAuthorization();
 app.MapControllers();
 
